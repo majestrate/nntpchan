758a2c6bbbaaf0e74aa3ae8113eae0155e39c9bb
jeff <ampernand@gmail.com>
2015-10-04 06:32:10 -0400

    newboard.js prepends 'overchan.' if it's not present in the name

--- contrib/static/newboard.js
+++ contrib/static/newboard.js
@@ -1,6 +1,10 @@
 function createBoard() {
   var form = document.getElementById("postform");
   var e = document.getElementById("boardname");
-  form.action = form.action + e.value;
+  var board = e.value;
+  if ( ! board.startsWith("overchan.") ) {
+    board = "overchan." + board;
+  }
+  form.action = form.action + board;
   form.submit();
 }
