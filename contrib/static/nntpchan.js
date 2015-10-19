4698f7a1186c3a3f9916f3ec5c44fe5564d20cc7
jeff <ampernand@gmail.com>
2015-10-19 09:20:30 -0400

    try fixing backlinks

--- contrib/static/nntpchan.js
+++ contrib/static/nntpchan.js
@@ -9,6 +9,6 @@ function nntpchan_backlink(shorthash)
   var elem = document.getElementById("postform_message");
   if ( elem )
   {
-    elem.value += ">>" + shorthash.substr(2,10) + "\n";
+    elem.value += ">>" + shorthash.substr(0,10) + "\n";
   }
 }
