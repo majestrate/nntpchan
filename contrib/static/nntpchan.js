98bb655a3c173894c73358e382c2da4d6aa95557
Jeff Becker <ampernand@gmail.com>
2016-01-20 11:42:18 -0500

    add banners

--- contrib/static/nntpchan.js
+++ contrib/static/nntpchan.js
@@ -12,3 +12,15 @@ function nntpchan_backlink(shorthash)
     elem.value += ">>" + shorthash.substr(0,10) + "\n";
   }
 }
+
+var banner_count = 3;
+
+// inject a banner into an element
+function nntpchan_inject_banners(elem, prefix) {
+  var n = Math.floor(Math.random() * banner_count);
+  var banner = prefix + "static/banner_"+n+".jpg";
+  var e = document.createElement("img");
+  e.src = banner;
+  e.setAttribute("class", "nntpchan_banner");
+  elem.appendChild(e);
+}
