2dea0917d11b4a0a5bd036d56efe20cc3e31dd2d
ZiRo <z_i_r_o@ymail.com>
2016-04-27 15:36:57 +0200

    Add inline webm playback

--- build-js.sh
+++ build-js.sh
@@ -14,6 +14,8 @@ fi
 
 echo -e "//For source code and license information please check https://github.com/majestrate/nntpchan \n" > ./contrib/static/nntpchan.js
 
+
+cat ./contrib/js/main.js_ | $GOPATH/bin/minify --mime=text/javascript >> ./contrib/static/nntpchan.js
 for f in ./contrib/js/*.js ; do
 	cat $f | $GOPATH/bin/minify --mime=text/javascript >> ./contrib/static/nntpchan.js
 done
