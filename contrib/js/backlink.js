f0fd23e6a9f3f8396242ec91829c74aab37244c5
Jeff Becker <ampernand@gmail.com>
2016-04-30 12:33:29 -0400

    more logging

--- contrib/js/backlink.js
+++ contrib/js/backlink.js
@@ -174,6 +174,7 @@ DynReply.prototype.setRoot = function(roothash) {
 
 // reply box function
 function nntpchan_reply(prefix, parent, shorthash) {
+  console.log(parent);
   if (prefix && parent && parent.roothash && parent.boardname) {
     var boardname = parent.boardname;
     var roothash = parent.roothash;
