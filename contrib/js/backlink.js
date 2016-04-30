480c40ce4fd358f472464da05b7b91f9ebf9c843
Jeff Becker <ampernand@gmail.com>
2016-04-30 13:26:42 -0400

    try fixing drag to move

--- contrib/js/backlink.js
+++ contrib/js/backlink.js
@@ -263,10 +263,11 @@ function init(prefix) {
   var e = rpl.elem;
   e.setAttribute("draggable", "true");
   e.ondragend = function(ev) {
-    console.log(ev);
     var el = document.getElementById("postform_container");
     el.style.top = "" + ev.y;
     el.style.left = "" + ev.x;
+    console.log(el);
+    ev.preventDefault();
   }
 }
 
