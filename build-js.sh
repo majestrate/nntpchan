cdcd0ce68da24cfe0589bbce41f04757604cf0a9
Jeff Becker <ampernand@gmail.com>
2016-04-30 09:58:21 -0400

    add mathjax submodule

--- build-js.sh
+++ build-js.sh
@@ -12,13 +12,26 @@ if [ ! -f $GOPATH/bin/minify ]; then
   echo "set up minifiy"  
 	go get -v github.com/tdewolff/minify/cmd/minify
 fi
+outfile=$(readlink -e ./contrib/static/nntpchan.js)
 
-echo -e "//For source code and license information please check https://github.com/majestrate/nntpchan \n" > ./contrib/static/nntpchan.js
+mini() {
+    echo "minify $1"
+    echo "" >> $2
+    echo "/* $1 */" >> $2
+    $GOPATH/bin/minify --mime=text/javascript >> $2 < $1
+}
 
-echo -n "minify "
-cat ./contrib/js/main.js_ | $GOPATH/bin/minify --mime=text/javascript >> ./contrib/static/nntpchan.js
+echo -e "//For source code and license information please check https://github.com/majestrate/nntpchan \n" > $outfile
+
+mini ./contrib/js/main.js_ $outfile
+
+if [ ! -e ./contrib/js/contrib/MathJax/ ] ; then
+    git submodule --init update .
+fi
+mini ./contrib/js/contrib/MathJax/MathJax.js $outfile
+
+# local js
 for f in ./contrib/js/*.js ; do
-  echo -n "."
-	cat $f | $GOPATH/bin/minify --mime=text/javascript >> ./contrib/static/nntpchan.js
+  mini $f $outfile
 done
 echo "ok"
