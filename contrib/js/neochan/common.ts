2aa79bd014613dd5f624627c7ddc912b1e66d529
Jeff Becker <ampernand@gmail.com>
2016-10-18 09:32:00 -0400

    add more stuff

---
+++ contrib/js/neochan/common.ts
@@ -0,0 +1,7 @@
+
+/** @brief type that is called to initialize a dom element */
+export interface Init
+{
+    initialize (elem : any);
+}
+
---
+++ contrib/js/neochan/postform.ts
@@ -0,0 +1,11 @@
+
+import { Init } from "./common"
+
+
+export class Postform implements Init
+{
+    initialize(elem : any) {
+        
+    }
+}
+
