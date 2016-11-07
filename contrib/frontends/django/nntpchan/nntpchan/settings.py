78dccbfd610ab9a7ace6aaf27a81512a1a7c76e1
Jeff Becker <ampernand@gmail.com>
2016-11-07 09:56:58 -0500

    make secret_key "changeme" by default to encourage people to change it

--- contrib/frontends/django/nntpchan/nntpchan/settings.py
+++ contrib/frontends/django/nntpchan/nntpchan/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%2!hvzm=5$(##&cplgh*3fn&rlmvn+$8^@q8z7!p-vazpr*bjy'
-
+SECRET_KEY = 'changeme'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
