f94ecbdaa990ac80cea6d85e6b34cfb7b0319f9c
Jeff Becker <ampernand@gmail.com>
2017-01-28 15:12:00 -0500

    add psytext alias in lua

--- contrib/lua/memeposting.lua
+++ contrib/lua/memeposting.lua
@@ -13,9 +13,14 @@ function explode_text(str)
   return span_text("explode", str)
 end
 
+function psy_text(str)
+  return span_text("psy", str)
+end
+
 -- generate extra markup
 function memeposting(body, prefix)
   body = string.gsub(body, "|(.-)|", wobble_text)
   body = string.gsub(body, "//(.-)\\\\", explode_text)
+  body = string.gsub(body, "/@(.-)@\\", psy_text)
   return body
 end
