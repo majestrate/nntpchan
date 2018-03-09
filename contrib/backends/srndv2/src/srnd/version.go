3899989f2ee01bd73696f1620dfe942ff703b5d7
Jeff Becker <jeff@i2p.rocks>
2018-03-09 10:57:31 -0500

    bump version

--- contrib/backends/srndv2/src/srnd/version.go
+++ contrib/backends/srndv2/src/srnd/version.go
@@ -6,12 +6,13 @@ package srnd
 
 import "fmt"
 
-const major_version = 5
-const minor_version = 1
+const major_version = 3
+const minor_version = 0
+const patch_verson = 0
 const program_name = "srnd"
 
 var GitVersion string
 
 func Version() string {
-	return fmt.Sprintf("%s-2.%d.%d%s", program_name, major_version, minor_version, GitVersion)
+	return fmt.Sprintf("%s-%d.%d.%d%s", program_name, major_version, minor_version, patch_verson, GitVersion)
 }
