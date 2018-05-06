657b285375de001af6562da4b58da8c83707fe8a
Jeff Becker <jeff@i2p.rocks>
2018-05-06 09:49:29 -0400

    more

--- contrib/backends/nntpchan-daemon/include/nntpchan/message.hpp
+++ contrib/backends/nntpchan-daemon/include/nntpchan/message.hpp
@@ -10,6 +10,7 @@ struct MessageDB
 {
   using BoardPage = nntpchan::model::BoardPage;
   using Thread = nntpchan::model::Thread;
+  virtual ~MessageDB() {};
   virtual bool LoadBoardPage(BoardPage &board, const std::string &newsgroup, uint32_t perpage, uint32_t page) const = 0;
   virtual bool FindThreadByHash(const std::string &hashhex, std::string &msgid) const = 0;
   virtual bool LoadThread(Thread &thread, const std::string &rootmsgid) const = 0;
